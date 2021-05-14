c8f696a9f59d0d14e15a01becc3f2a0195a9a510
Dennis <dennis.melamed1@gmail.com>
2021-05-14 09:29:45 -0400

    submission

---
+++ triplet_loss_datamodule.py
@@ -0,0 +1,60 @@
+import pytorch_lightning as pl
+import torch
+import random
+import numpy as np
+import glob
+from torch.utils.data import random_split, DataLoader
+
+from torchvision import transforms
+from triplet_loss_dataset import TripletDataset 
+
+class ZeroShotDataModule(pl.LightningDataModule):
+
+    def __init__(self, batch_size = 32, train_plants = 'all', test_plants = 'all', data_dir = "PlantVillage-Dataset"):
+        super().__init__()
+        torch.manual_seed(0)
+        random.seed(0)
+        np.random.seed(0)
+        self.batch_size = batch_size
+        
+        self.data_dir = data_dir
+        im_folders = glob.glob(data_dir + "/*/")
+        self.all_plants = sorted(list(set([a[:-1].split("/")[1].split("___")[0] for a in im_folders])))
+        for plant in self.all_plants:
+            if data_dir + "/" + plant + "___healthy/" not in im_folders:
+                self.all_plants.remove(plant)
+        if train_plants != 'all':
+            self.train_plants = sorted(list(set(train_plants).intersection(set(self.all_plants))))
+        else:
+            self.train_plants = self.all_plants
+        if test_plants != 'all':
+            self.test_plants = sorted(list(set(test_plants).intersection(set(self.all_plants))))
+        else:
+            self.test_plants = self.all_plants
+
+
+    def setup(self, stage=None):
+        if stage == 'fit' or stage is None:
+            self.train_data = TripletDataset(plant_types=self.train_plants, split='train', transform = True)
+            self.val_data = TripletDataset(plant_types=self.train_plants, split= 'val', transform = False)
+            print("image dimensions: {}".format(self.val_data.dims))
+            self.dims = self.train_data.dims
+
+        if stage == 'test' or stage is None:
+            ## TODO change to make a test dataset for each plant type, plus the first one contains all plant types
+            self.test_data_full = TripletDataset(plant_types=self.test_plants, split = "test", transform = False)
+            self.test_data_per_plant = {plant_name:TripletDataset(plant_types=[plant_name], split = "test", transform = False) for plant_name in self.test_plants}
+
+
+    def train_dataloader(self):
+        return DataLoader(self.train_data, batch_size=self.batch_size, num_workers = 24, shuffle = True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_data, batch_size=self.batch_size, num_workers = 24)
+
+    def test_dataloader(self):
+        # TODO return a dataloader for each test plant type
+        return DataLoader(self.test_data_full, batch_size=self.batch_size, num_workers = 24)
+    
+    def test_indiv_plants_dataloaders(self):
+        return {k:DataLoader(v, batch_size=self.batch_size, num_workers = 24) for k,v in self.test_data_per_plant.items()}
---
+++ triplet_loss_dataset.py
@@ -0,0 +1,170 @@
+from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as T
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.utils import make_grid
+from torchvision.utils import save_image
+from IPython.display import Image
+import matplotlib.pyplot as plt 
+import numpy as np
+import random
+import glob
+from PIL import Image
+from typing import Any, Callable, cast, Dict, List, Optional, Tuple
+import torchvision.transforms.functional as TF
+import os.path as osp 
+
+
+class TripletDataset(Dataset):
+
+
+    dataset = None
+    full_class_mapping = None
+    healthy_images = None
+    dims = None
+
+    every_im_transform = transforms.Compose([
+        transforms.Resize((224,224)),  # interpolation `BILINEAR` is applied by default
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(0.4),
+        transforms.RandomVerticalFlip(0.4),
+        transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+        transforms.RandomPerspective(),
+        transforms.RandomRotation(180),
+        transforms.RandomCrop(224,224)
+
+    ])
+    def __init__(self, data_dir = "PlantVillage-Dataset", plant_types = 'all', transform = False, split = "train"):
+        self.data_dir = data_dir
+        self.split = split
+        im_folders = glob.glob(data_dir + "/*/")
+        self.im_folders = im_folders
+        self.all_plants = sorted(list(set([a[:-1].split("/")[1].split("___")[0] for a in im_folders])))
+        for plant in self.all_plants:
+            if data_dir + "/" + plant + "___healthy/" not in im_folders:
+                self.all_plants.remove(plant)
+        if plant_types != 'all':
+            self.all_plants = sorted(list(set(plant_types).intersection(set(self.all_plants))))
+        print("plants in this dataset: {}".format(self.all_plants))
+        self.weights = {a:{"healthy":0, "unhealthy":0} for a in self.all_plants}
+        self.get_paths_and_full_classes()
+        #print("weights: {}".format(self.weights))
+        print("\n")
+        if not transform:
+            self.transform = None
+
+
+
+
+    def get_paths_and_full_classes(self):
+        dataset = []
+        healthy_images = {plant:[] for plant in self.all_plants}
+        unhealthy_images = {plant:[] for plant in self.all_plants}
+        for plant in self.all_plants:
+            diseases = [a.split("/")[-1] for a in glob.glob(osp.join(self.data_dir, "splits", plant, "*"))]
+            for disease in diseases:
+                with open(osp.join(self.data_dir, "splits", plant, disease, self.split + ".txt"), 'r') as f:
+                    images = f.readlines()
+
+                #print("{}, {}: {}".format(plant, disease, len(images)) )
+                self.weights[plant]["healthy" if disease=="healthy" else "unhealthy"] += len(images)
+
+                for image in images:
+                    dataset.append({"path":image.strip(),
+                                     "plant": plant,
+                                     "disease": disease,
+                                      "healthy": 1 if disease=="healthy" else 0
+                                       })
+                    if dataset[-1]["healthy"] == 1:
+                        healthy_images[dataset[-1]["plant"]].append(dataset[-1]['path'])
+                    else:
+                        unhealthy_images[dataset[-1]['plant']].append(dataset[-1]['path'])
+
+        self.dataset = dataset
+        self.healthy_images = healthy_images
+        self.unhealthy_images = unhealthy_images
+        self.dims = self.pil_loader(dataset[-1]['path']).size
+
+        weight_sum = np.sum([1/self.weights[a][b] for a in self.all_plants for b in ['healthy', 'unhealthy']])
+        #print("weight sum: {}".format(weight_sum))
+        self.weights = {plant:{"healthy":1/(self.weights[plant]['healthy']*weight_sum),
+                               "unhealthy":1/(self.weights[plant]['unhealthy']*weight_sum) } for plant in self.all_plants}
+
+    IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
+
+    @classmethod
+    def pil_loader(cls, path: str) -> Image.Image:
+        # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+        with open(path, 'rb') as f:
+            img = Image.open(f)
+            return img.convert('RGB')
+
+
+    # TODO: specify the return type
+    @classmethod
+    def accimage_loader(cls, path: str) -> Any:
+        import accimage
+        try:
+            return accimage.Image(path)
+        except IOError:
+            # Potentially a decoding problem, fall back to PIL.Image
+            return pil_loader(path)
+
+    @classmethod
+    def default_loader(cls, path: str) -> Any:
+        from torchvision import get_image_backend
+        if get_image_backend() == 'accimage':
+            return cls.accimage_loader(path)
+        else:
+            return cls.pil_loader(path)
+
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        plant_name = self.dataset[idx]['plant']
+        healthy_plant_type = np.random.choice(self.all_plants)
+        unhealthy_plant_type = np.random.choice(self.all_plants)
+        
+        healthy_image_fn = self.healthy_images[healthy_plant_type][np.random.randint(len(self.healthy_images[healthy_plant_type]))]
+        unhealthy_image_fn = self.unhealthy_images[unhealthy_plant_type][np.random.randint(len(self.unhealthy_images[unhealthy_plant_type]))]
+        unknown_image_fn = self.dataset[idx]['path']
+
+        healthy_image = self.default_loader(healthy_image_fn)
+        unhealthy_image = self.default_loader(unhealthy_image_fn)
+        unknown_image = self.default_loader(unknown_image_fn)
+        
+        
+        label = self.dataset[idx]['healthy']
+
+
+        if self.transform is not None:
+            healthy_image = self.transform(healthy_image)
+            unhealthy_image = self.transform(unhealthy_image)
+            unknown_image = self.transform(unknown_image)
+
+        healthy_tensor = self.every_im_transform(healthy_image)
+        unhealthy_tensor = self.every_im_transform(unhealthy_image)
+        unknown_tensor = self.every_im_transform(unknown_image)
+        
+        if label == 1:
+            positive_tensor = healthy_tensor
+            negative_tensor = unhealthy_tensor
+        else:
+            positive_tensor = unhealthy_tensor
+            negative_tensor = healthy_tensor
+
+        weight = self.weights[plant_name]['healthy' if label==1 else 'unhealthy']
+        
+        return {"positive image": positive_tensor,
+                "negative image": negative_tensor,
+                "unknown image": unknown_tensor,
+                "weight": weight,
+                "plant name": plant_name,
+                "label": float(label)}
